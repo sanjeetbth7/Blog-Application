e5c6f3c6c63ecd9eb3c746af234cb026f8bc353e
Sanjeet Kumar <sanjeetbth7@gmail.com>
2024-07-28 16:06:13 +0530

    create a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+const test = (req,res)=>{
+    res.json({message : "API is working!.."})
+};
+
+export default test;
--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import userRoutes from './routes/user.route.js';
+
 // Load environment variables from .env file
 dotenv.config();
 const app = express();
@@ -17,3 +19,5 @@ mongoose.connect(process.env.DBLINK)
 app.listen(PORT,()=>{
     console.log("App is running on port 3000....")
 })
+
+app.use('/api/user', userRoutes);
