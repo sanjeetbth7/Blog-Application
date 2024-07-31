70ffb5fe87cf9733461e322def4a1d5e81e224e1
Sanjeet Kumar <sanjeetbth7@gmail.com>
2024-07-31 19:18:20 +0530

    upload imgage functionality via firebase

--- client/src/Components/DashSidebar.jsx
+++ client/src/Components/DashSidebar.jsx
@@ -26,6 +26,7 @@ export default function DashSidebar() {
               icon={HiUser}
               label="User"
               labelColor="dark"
+              as='div'
             >
               Profile
             </Sidebar.Item>
