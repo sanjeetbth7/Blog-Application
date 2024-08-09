1009638adf81ececef0f4f52a3c1f315e35c0061
Sanjeet Kumar <sanjeetbth7@gmail.com>
2024-08-09 22:42:42 +0530

    delete comment function added

--- client/src/Components/Comment.jsx
+++ client/src/Components/Comment.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 import { Button, Textarea } from 'flowbite-react';
 
 
-export default function Comment({ comment, onLike , onEdit }) {
+export default function Comment({ comment, onLike , onEdit , onDelete}) {
   const [user, setUser] = useState({});
   const { currentUser } = useSelector((state) => state.user);
   const [isEditing, setIsEditing] = useState(false);
@@ -118,13 +118,22 @@ export default function Comment({ comment, onLike , onEdit }) {
               </p>
               {currentUser &&
                 (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                  <button
-                    type='button'
-                    onClick={handleEdit}
-                    className='text-gray-400 hover:text-blue-500'
-                  >
-                    Edit
-                  </button>
+                  <>
+                    <button
+                      type='button'
+                      onClick={handleEdit}
+                      className='text-gray-400 hover:text-blue-500'
+                    >
+                      Edit
+                    </button>
+                    <button
+                      type='button'
+                      onClick={() => onDelete(comment._id)}
+                      className='text-gray-400 hover:text-red-500'
+                    >
+                      Delete
+                    </button>
+                  </>
                 )}
             </div>
           </>
