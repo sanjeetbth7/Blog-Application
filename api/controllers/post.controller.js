5392e6a5cffdb93821f907192fd0afb31ebf3af1
Sanjeet Kumar <sanjeetbth7@gmail.com>
2024-08-09 12:00:34 +0530

    added delete post fuction

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -8,7 +8,7 @@ export const create = async (req, res, next) => {
   if (!req.body.title || !req.body.content) {
     return next(errorHandler(400, 'Please provide all required fields'));
   }
-  
+
   const slug = req.body.title
     .split(' ')
     .join('-')
@@ -68,6 +68,41 @@ export const getposts = async (req, res, next) => {
         totalPosts,
         lastMonthPosts,
       });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const deletepost = async (req, res, next) => {
+    if (!req.user.isAdmin || req.user.userId !== req.params.userId) {
+      return next(errorHandler(403, 'You are not allowed to delete this post'));
+    }
+    try {
+      await Post.findByIdAndDelete(req.params.postId);
+      res.status(200).json('The post has been deleted');
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const updatepost = async (req, res, next) => {
+    if (!req.user.isAdmin || req.user.userId !== req.params.userId) {
+      return next(errorHandler(403, 'You are not allowed to update this post'));
+    }
+    try {
+      const updatedPost = await Post.findByIdAndUpdate(
+        req.params.postId,
+        {
+          $set: {
+            title: req.body.title,
+            content: req.body.content,
+            category: req.body.category,
+            image: req.body.image,
+          },
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedPost);
     } catch (error) {
       next(error);
     }
