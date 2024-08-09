3fdb36f429116deaf7793647f7b27b55abb0f0cd
Sanjeet Kumar <sanjeetbth7@gmail.com>
2024-08-09 23:55:30 +0530

    Home page added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
-import { CallToAction } from '../Components'
+import { CallToAction,PostCard} from '../Components'
+import { Link } from 'react-router-dom';
+
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/post/getPosts');
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
   return (
+    <>
     <div>
-      <div>Home</div>
-      <CallToAction />
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
+        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
+        <p className='text-gray-500 text-xs sm:text-sm'>
+          Here you'll find a variety of articles and tutorials on topics such as
+          web development, software engineering, and programming languages.
+        </p>
+        <Link
+          to='/search'
+          className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+        <CallToAction />
+      </div>
     </div>
-  )
+
+    <div className='flex flex-col justify-center items-center mb-5'>
+        <h1 className='text-xl mt-5'>Recent articles</h1>
+        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+          {posts &&
+            posts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
+        <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              View all posts
+            </Link>
+      </div>
+    </>
+  );
 }
 
