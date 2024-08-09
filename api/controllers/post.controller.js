f0aca93b72c7b99dfc6eb772593048ab55d2a000
Sanjeet Kumar <sanjeetbth7@gmail.com>
2024-08-09 11:47:34 +0530

    added show more on admin dashposts

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -8,6 +8,7 @@ export const create = async (req, res, next) => {
   if (!req.body.title || !req.body.content) {
     return next(errorHandler(400, 'Please provide all required fields'));
   }
+  
   const slug = req.body.title
     .split(' ')
     .join('-')
@@ -22,7 +23,7 @@ export const create = async (req, res, next) => {
     const savedPost = await newPost.save();
     res.status(201).json(savedPost);    
   } catch (error) {
-    // console.log(error);
+    console.log(error);
     next(error);
   }
 };
