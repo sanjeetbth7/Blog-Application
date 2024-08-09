e605364be4175711d800856e434f5c0f3c86dffe
Sanjeet Kumar <sanjeetbth7@gmail.com>
2024-08-09 20:29:20 +0530

    Show all comments related to the post at post page

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { deleteUser, signout, test, updateUser, getUsers } from '../controllers/user.controller.js';
+import { deleteUser, signout, test, updateUser, getUser, getUsers } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken,updateUser);
 router.delete('/delete/:userId', verifyToken,deleteUser);
 router.post('/signout', signout);
-router.get('/getusers', verifyToken, getUsers);
+router.get('/getusers', verifyToken, getUsers); // for admin view
+router.get('/:userId', getUser); // for comments
+
 export default router;
