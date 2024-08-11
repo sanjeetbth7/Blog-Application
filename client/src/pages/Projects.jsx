9cf9cf8999244a32c9292bf516c9eadd84845722
Sanjeet Kumar <sanjeetbth7@gmail.com>
2024-08-11 21:28:32 +0530

    modified banner and carousel

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { CallToAction } from "../Components";
 export default function Projects() {
   return (
-    <div className=" min-h-screen">
+    <div className="mb-4">
       <div className="max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
         <h1 className="text-3xl font-semibold">Pojects</h1>
         <p className="text-md text-gray-500">
