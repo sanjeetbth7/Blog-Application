715440631d5db41a629a1ac37420270a9264318f
Sanjeet Kumar <sanjeetbth7@gmail.com>
2025-03-05 19:23:08 +0530

    fixed spelling mistake in Project page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -4,7 +4,7 @@ export default function Projects() {
   return (
     <div className="mb-4">
       <div className="max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
-        <h1 className="text-3xl font-semibold">Pojects</h1>
+        <h1 className="text-3xl font-semibold">Projects</h1>
         <p className="text-md text-gray-500">
           Build fun and engaging projects while learning Web development!
         </p>
